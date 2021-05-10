c103dd5abbdc24592eeba4ff24846579e3adbc14
Karlven <karlven@outlook.com>
2021-05-11 00:10:35 +0800

    test

---
+++ utils/str_utils_test.go
@@ -0,0 +1,13 @@
+package utils
+
+import (
+	"testing"
+)
+
+func TestStrToFloat64(t *testing.T) {
+	val := 3434.4455664545
+	res := Float64ToStr(val)
+	t.Log(res)
+	val, _ = StrToFloat64(res)
+	t.Log(val)
+}
