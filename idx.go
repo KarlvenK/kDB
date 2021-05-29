b433f5d812ee5ab133c5c8e3aa2912e255cacdfc
Karlven <karlven@outlook.com>
2021-05-30 00:01:34 +0800

    add operations of list idx

--- idx.go
+++ idx.go
@@ -1,7 +1,10 @@
 package kDB
 
 import (
+	"github.com/KarlvenK/kDB/ds/list"
 	"github.com/KarlvenK/kDB/index"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -69,5 +72,50 @@ func (db *kDB) buildStringIndex(idx *index.Indexer, opt uint16) {
 		db.strIndex.idxList.Put(idx.Meta.Key, idx)
 	case StringRem:
 		db.strIndex.idxList.Remove(idx.Meta.Key)
+	}
+}
+
+// buildListIndex build list indexes
+func (db *kDB) buildListIndex(idx *index.Indexer, opt uint16) {
+	if db.listIndex == nil || idx == nil {
+		return
+	}
+
+	key := string(idx.Meta.Key)
+	switch opt {
+	case ListLPush:
+		db.listIndex.indexes.LPush(key, idx.Meta.Value)
+	case ListLPop:
+		db.listIndex.indexes.LPop(key)
+	case ListRPush:
+		db.listIndex.indexes.RPush(key, idx.Meta.Value)
+	case ListRPop:
+		db.listIndex.indexes.RPop(key)
+	case ListLRem:
+		if count, err := strconv.Atoi(string(idx.Meta.Extra)); err == nil {
+			db.listIndex.indexes.LRem(key, idx.Meta.Value, count)
+		}
+	case ListLInsert:
+		extra := string(idx.Meta.Extra)
+		s := strings.Split(extra, ExtraSeparator)
+		if len(s) == 2 {
+			pivot := []byte(s[0])
+			if opt, err := strconv.Atoi(s[1]); err == nil {
+				db.listIndex.indexes.LInsert(string(idx.Meta.Key), list.InsertOption(opt), pivot, idx.Meta.Value)
+			}
+		}
+	case ListLSet:
+		if i, err := strconv.Atoi(string(idx.Meta.Extra)); err == nil {
+			db.listIndex.indexes.LSet(key, i, idx.Meta.Value)
+		}
+	case ListLTrim:
+		extra := string(idx.Meta.Extra)
+		s := strings.Split(extra, ExtraSeparator)
+		if len(s) == 2 {
+			start, _ := strconv.Atoi(s[0])
+			end, _ := strconv.Atoi(s[1])
+
+			db.listIndex.indexes.LTrim(string(idx.Meta.Key), start, end)
+		}
 	}
 }
